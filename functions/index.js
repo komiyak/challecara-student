be1181730d3e76b308acee5a9deefe3a0b3a0fe6
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-05 21:02:19 +0900

    Add a callable function.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
+});
+
+exports.addMessage = functions.https.onCall((data, context) => {
+    return {message: "addMessage"};
 });
