f4e04b738d9eadfc3c3a59c480eba1578ec47997
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-10 11:24:10 +0900

    Sync records of students with Firestore data source.

--- functions/index.js
+++ functions/index.js
@@ -86,7 +86,7 @@ exports.syncStudentRecords = functions.https.onRequest(async (request, response)
         ranges: ranges
     });
 
-    const students = admin.firestore().collection('students');
+    const schools = admin.firestore().collection('schools');
 
     if (batchGetRes.data.valueRanges) {
         for (let i = 0; i < batchGetRes.data.valueRanges.length; i++) {
@@ -97,7 +97,7 @@ exports.syncStudentRecords = functions.https.onRequest(async (request, response)
 
             if (uid && name && phoneticName) {
                 // eslint-disable-next-line no-await-in-loop
-                await students.doc(uid).set({
+                await schools.doc(uid).set({
                     name: name,
                     phoneticName: phoneticName
                 });
@@ -105,7 +105,54 @@ exports.syncStudentRecords = functions.https.onRequest(async (request, response)
         }
     }
 
-    response.send("OK student!");
+    // 学生データの同期
+    const studentRes = await sheets.spreadsheets.values.get({
+        auth: client,
+        spreadsheetId: functions.config().student_record.spreadsheet_id,
+        range: '2019!A2:A200'
+    });
+    let studentRanges = [];
+    for (let i = 0; i < studentRes.data.values.length; i++) {
+        const line = 2 + i;
+        studentRanges.push(`2019!A${line}:H${line}`);
+    }
+    const studentBatchGetRes = await sheets.spreadsheets.values.batchGet({
+        auth: client,
+        spreadsheetId: functions.config().student_record.spreadsheet_id,
+        ranges: studentRanges
+    });
+
+    const students = admin.firestore().collection('students');
+
+    if (studentBatchGetRes.data.valueRanges) {
+        for (let i = 0; i < studentBatchGetRes.data.valueRanges.length; i++) {
+            const values = studentBatchGetRes.data.valueRanges[i].values[0];
+            const uid = values[0];
+            const school = values[1];
+            const sei = values[2];
+            const mei = values[3];
+            const phoneticSei = values[4];
+            const phoneticMei = values[5];
+            const email = values[6];
+            const phone = values[7];
+
+            if (uid && school && sei && mei && phoneticSei && phoneticMei) {
+                // eslint-disable-next-line no-await-in-loop
+                await students.doc(uid).set({
+                    school: school,
+                    year: '2019',
+                    sei: sei,
+                    mei: mei,
+                    phoneticSei: phoneticSei,
+                    phoneticMei: phoneticMei,
+                    email: email,
+                    phone: phone
+                });
+            }
+        }
+    }
+
+    response.status(204).end();
 });
 
 // @data code (required) The authorization code from LINE Login.
