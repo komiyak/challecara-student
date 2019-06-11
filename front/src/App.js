9d74912c393716fea34d5b01eb74d05036a05770
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-11 23:31:36 +0900

    Clean up the html.

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import btnLineLoginBase from 'images/1x/btn_line_login_base.png';
 import 'App.scss';
 
 function App() {
     return (
         <div className="App container">
-            <h1 className="title">Challecara Student</h1>
+            <h1 className="title">Challecara!</h1>
 
             <div className="content">
-                <p>チャレキャラへようこそ！</p>
+                <p>チャレキャラ2019へようこそ！</p>
 
                 <p>
-                    <strong>学校名</strong><br/>
-                    ○○大学
-                </p>
-
-                <p>
-                    <strong>あなたの名前</strong><br/>
+                    <strong>あなたのお名前</strong><br/>
                     田中 太郎
                 </p>
             </div>
@@ -25,15 +19,10 @@ function App() {
                 セットアップを開始しますので、LINE アカウントでログインをお願いします。
             </div>
 
-            <a className="button is-medium">
-                <span className="icon">
-                    <i className="fab fa-line"></i>
-                </span>
-                <span>LINEでログイン</span>
-            </a>
-
-            <img src={btnLineLoginBase} alt="Button"/>
-            <a className="buttonLineLogin">LINE Login</a>
+            <p>
+                {/* eslint-disable-next-line */}
+                <a className="buttonLineLogin" href='#'>LINE Login</a>
+            </p>
         </div>
     );
 }
