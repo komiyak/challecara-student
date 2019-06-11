e0f92c99fc1c754bee3d21bc8c7cb7f0710a710f
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-11 16:56:35 +0900

    Add the redux-logger.

--- front/src/index.js
+++ front/src/index.js
@@ -7,9 +7,15 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import {createLogger} from "redux-logger";
+import reducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 ReactDOM.render(
     <Provider store={store}>
