05ca0ad74a89b4224034044f293fcd6c54bafaf5
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-11 14:27:09 +0900

    Rename the student-front to the front.

--- build_front.sh
+++ build_front.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Building the student-front app.
-pushd student-front; npm run build; popd
+# Building the frontend app.
+pushd front; npm run build; popd
 # Replace the public directory to the built app.
 rm -rf public/
-mv student-front/build/ public/
+mv front/build/ public/
 
 # Keep .keep file.
 touch public/.keep
