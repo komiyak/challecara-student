c01c2716aed3cfe63eac2b301512eecfa3483f60
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-12 22:33:00 +0900

    Rename 'front' to 'web'.

--- build_app.sh
+++ build_app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Building the frontend app.
-pushd front; npm run build; popd
+pushd web; npm run build; popd
 # Replace the public directory to the built app.
 rm -rf public/
-mv front/build/ public/
+mv web/build/ public/
 
 # Keep .keep file.
 touch public/.keep
--- reinstall_packages.sh
+++ reinstall_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pushd functions; npm cache clean --force; rm -rf node_modules; npm install; popd
-pushd front; npm cache clean --force; rm -rf node_modules; npm install; popd
+pushd web; npm cache clean --force; rm -rf node_modules; npm install; popd
