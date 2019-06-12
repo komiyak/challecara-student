d4565574c446dc9a22a6bd782240ece391bd3358
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-12 18:19:24 +0900

    Clean up the functions and the front directory.

--- reinstall_packages.sh
+++ reinstall_packages.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-pushd functions
-npm cache clean --force
-rm -rf node_modules
-npm install
-popd
+pushd functions; npm cache clean --force; rm -rf node_modules; npm install; popd
+pushd front; npm cache clean --force; rm -rf node_modules; npm install; popd
