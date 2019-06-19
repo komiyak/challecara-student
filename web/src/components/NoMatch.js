b43a89ab2f70fce88c442d87422493a04e8f3ef5
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-19 16:10:59 +0900

    Fix problems from the lintation.

--- web/src/components/NoMatch.js
+++ web/src/components/NoMatch.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-const NoMatch = ({location}) => (
-    <div className='container'>
-        <h1 className="title is-3">No match for <code>{location.pathname}</code></h1>
-    </div>
-);
+const NoMatch = ({ location }) => (
+  <div className='container'>
+    <h1 className="title is-3">No match for <code>{location.pathname}</code></h1>
+  </div>
+)
 
-export default NoMatch;
+export default NoMatch
