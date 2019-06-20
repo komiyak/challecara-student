77d9c251380e3b8b12d7c3533bbbb73c6cc44944
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-20 21:14:22 +0900

    Add a button for adding the friend.

--- web/src/containers/NewcomerOAuthSuccess.js
+++ web/src/containers/NewcomerOAuthSuccess.js
@@ -3,9 +3,16 @@ import { connect } from 'react-redux'
 
 class NewcomerOAuthSuccess extends React.Component {
   render() {
+    // noinspection HtmlDeprecatedAttribute
     return (
       <div>
         <p>Success.</p>
+
+        <div>
+          <a href="http://nav.cx/87iSP2h">
+            <img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"/>
+          </a>
+        </div>
       </div>
     )
   }
