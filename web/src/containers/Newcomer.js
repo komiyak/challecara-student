08dc84cef82ac9ccea0bf5c90701a935987b10d3
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-21 22:22:04 +0900

    Add a slideshow to the background with jQury and a plugin.

--- web/src/containers/Newcomer.js
+++ web/src/containers/Newcomer.js
@@ -5,6 +5,8 @@ import '../sass/Newcomer.scss'
 
 import { newcomerController } from '../redux/modules/newcomerController'
 
+const $ = window.jQuery
+
 class Newcomer extends React.Component {
   static propTypes = {
     isFetching: PropTypes.bool.isRequired,
@@ -15,34 +17,53 @@ class Newcomer extends React.Component {
 
   componentDidMount() {
     this.props.dispatch(newcomerController.fetchStudent(this.props.match))
+
+    this.$el = $(this.el)
+    this.$el.vegas({
+      slides: [
+        { src: 'https://s3-us-west-2.amazonaws.com/dev.komiyak.com/challecara-student-app/slideshow/photo01.jpg' },
+        { src: 'https://s3-us-west-2.amazonaws.com/dev.komiyak.com/challecara-student-app/slideshow/photo02.jpg' },
+        { src: 'https://s3-us-west-2.amazonaws.com/dev.komiyak.com/challecara-student-app/slideshow/photo03.jpg' },
+        { src: 'https://s3-us-west-2.amazonaws.com/dev.komiyak.com/challecara-student-app/slideshow/photo04.jpg' }
+      ],
+      overlay: 'https://s3-us-west-2.amazonaws.com/dev.komiyak.com/challecara-student-app/slideshow/overlays/07.png'
+    })
+  }
+
+  componentWillUnmount() {
+    this.$el.vegas('destroy')
   }
 
   render() {
     return (
-      <div className="App container">
-        <h1 className="title">Challecara!</h1>
+      <div className='component-root'>
+        <div ref={el => this.el = el} className='full-height-background'/>
 
-        <div className="content">
-          <p>チャレキャラ2019へようこそ！</p>
-          <p>
-            <strong>あなたのお名前</strong><br/>
-            {this.props.isFetching
-              ? <span><i className="fas fa-spinner fa-pulse"/> Loading...</span> : this.props.studentName
+        <div className="component-body">
+          <div className='container App'>
+            <h1 className="title">Challecara!</h1>
+
+            <div className="content">
+              <p>To a maker</p>
+              <p>
+                {this.props.isFetching
+                  ? <span><i className="fas fa-spinner fa-pulse"/> Loading...</span> : this.props.studentName
+                }
+              </p>
+            </div>
+
+            {!this.props.isFetching &&
+            <div>
+              <div className="content">
+                セットアップを開始しますので、LINE アカウントでログインをお願いします。
+              </div>
+
+              {/* eslint-disable-next-line */}
+              <p><a className="buttonLineLogin" href={this.props.url ? this.props.url : ""}>LINE Login</a></p>
+            </div>
             }
-          </p>
-        </div>
-
-        {!this.props.isFetching &&
-        <div>
-          <div className="content">
-            セットアップを開始しますので、LINE アカウントでログインをお願いします。
           </div>
-
-          {/* eslint-disable-next-line */}
-          <p><a className="buttonLineLogin" href={this.props.url ? this.props.url : ""}>LINE Login</a></p>
         </div>
-        }
-
       </div>
     )
   }
