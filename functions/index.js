4d53648fd2cc386dbbce0560d7f428862ef94b6a
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-26 16:54:30 +0900

    Remove the prompt of the bot.

--- functions/index.js
+++ functions/index.js
@@ -49,8 +49,7 @@ exports.getOAuthUrl = functions.https.onCall(async (data) => {
             `&client_id=${functions.config().line_login.client_id}` +
             `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
             `&state=${encodedState}` +
-            `&scope=openid%20profile%20email` +
-            `&bot_prompt=aggressive`
+            `&scope=openid%20profile%20email`
     };
 });
 
